7532d1f72fa5d645ca4df7110dcbee32f7dade3f
outbreaker <outbreaker@outbreaker.org>
2020-10-10 22:25:48 +0200

    Add Factory Test

---
+++ src/test/java/de/gematik/demis/ui/value/editor/ValueTypeEditorFactoryTest.java
@@ -0,0 +1,22 @@
+package de.gematik.demis.ui.value.editor;
+
+import de.gematik.demis.entities.VALUE_TYPE;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+class ValueTypeEditorFactoryTest {
+
+    @Test
+    void createEditor() {
+        Assert.assertEquals(BooleanEditor.class, ValueTypeEditorFactory.createEditor(VALUE_TYPE.BOOLEAN).getClass());
+        Assert.assertEquals(IntEditor.class, ValueTypeEditorFactory.createEditor(VALUE_TYPE.INT).getClass());
+        Assert.assertEquals(StringListEditor.class, ValueTypeEditorFactory.createEditor(VALUE_TYPE.STRING_LIST).getClass());
+        Assert.assertEquals(RelativPathEditor.class, ValueTypeEditorFactory.createEditor(VALUE_TYPE.RELATIVE_PATH).getClass());
+        Assert.assertEquals(EmptyEditor.class, ValueTypeEditorFactory.createEditor(VALUE_TYPE.STRING).getClass());
+        Assert.assertEquals(EmptyEditor.class, ValueTypeEditorFactory.createEditor(VALUE_TYPE.PATH_LIST).getClass());
+        Assert.assertEquals(EmptyEditor.class, ValueTypeEditorFactory.createEditor(VALUE_TYPE.PATH).getClass());
+    }
+}
