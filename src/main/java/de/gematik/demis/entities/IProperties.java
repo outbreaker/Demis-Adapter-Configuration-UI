9426995053f4df0363ed11a24f9079797e3f5574
outbreaker <outbreaker@outbreaker.org>
2020-10-11 22:08:01 +0200

    extract interface for properties enums

---
+++ src/main/java/de/gematik/demis/entities/IProperties.java
@@ -0,0 +1,15 @@
+package de.gematik.demis.entities;
+
+public interface IProperties {
+    String getKey();
+
+    VALUE_TYPE getType();
+
+    boolean isOptional();
+
+    String getDisplayName();
+
+    String getToolTip();
+
+    String getDefaultValue();
+}
