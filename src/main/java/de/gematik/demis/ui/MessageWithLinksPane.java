14aa08db62c45c2db9f81ab5e468e4a6a9705357
Ilena Puls <ilena.puls@gematik.de>
2020-10-26 19:48:07 +0100

    added message dialog with ability to integrate html links

---
+++ src/main/java/de/gematik/demis/ui/MessageWithLinksPane.java
@@ -0,0 +1,37 @@
+package de.gematik.demis.ui;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.io.IOException;
+import java.net.URI;
+import javax.swing.JEditorPane;
+import javax.swing.JLabel;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MessageWithLinksPane extends JEditorPane {
+
+  private static final long serialVersionUID = 1L;
+  private static Logger LOG = LoggerFactory.getLogger(MessageWithLinksPane.class.getName());
+
+  public MessageWithLinksPane(String htmlBody) {
+    super("text/html", "<html><body>" + htmlBody + "</body></html>");
+    addHyperlinkListener(new HyperlinkListener() {
+      @Override
+      public void hyperlinkUpdate(HyperlinkEvent e) {
+        if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
+          try {
+            java.awt.Desktop.getDesktop().browse(URI.create(e.getURL().toString()));
+          } catch (IOException ioException) {
+            LOG.debug("Error while opening link!");
+            // TODO ? Noch ein Fenster für Fehler, dass Browser nicht gefunden werden kann?
+          }
+        }
+      }
+    });
+    setEditable(false);
+    setBorder(null);
+  }
+}
