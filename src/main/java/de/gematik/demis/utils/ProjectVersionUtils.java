aca79d1893b16b7f83fe76521b5d65299e270e43
Ilena Puls <ilena.puls@gematik.de>
2020-10-27 17:38:35 +0100

    get version from version.txt for help dialog and build.gradle version

--- src/main/java/de/gematik/demis/utils/ProjectVersionUtils.java
+++ src/main/java/de/gematik/demis/utils/ProjectVersionUtils.java
@@ -1,27 +1,22 @@
 package de.gematik.demis.utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ProjectVersionUtils {
-  private static Logger LOG = LoggerFactory.getLogger(ImageUtils.class.getName());
+  private static Logger LOG = LoggerFactory.getLogger(ProjectVersionUtils.class.getName());
 
   private ProjectVersionUtils() {}
 
   public static String getProjectVersion() {
     String projectVersion = "";
-    Properties prop = new Properties();
-    InputStream input = null;
 
-    try {
-      input = new FileInputStream("build.gradle");
-      prop.load(input);
-      projectVersion = prop.getProperty("version");
-      LOG.debug("Version: " + prop.getProperty("version"));
+    try (InputStream input = ProjectVersionUtils.class.getClassLoader().getResourceAsStream("version.txt")){
+      projectVersion =  new Scanner(input).useDelimiter("\n").next();
+      LOG.debug("Version: " + projectVersion);
     } catch (IOException ex) {
       LOG.debug("Could not get Version!");
     }
